783709aed0ad0bcf870489645971afb39efc2f1c
Eltac E. Malikmammadov <Eltacem@code.local>
2022-04-14 15:26:15 +0400

    com-2

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 let btn = document.getElementById("fa-bars")
 let menu = document.getElementById("menu")
+let x = document.getElementById("fa-xmark")
 
 btn.addEventListener("click",()=>
 {
     menu.style.display = "block"
+    btn.style.display="none"
+    x.style.display="block"
+})
+
+x.addEventListener("click",()=>
+{
+    menu.style.display = "none"
+    btn.style.display="block"
+    x.style.display="none"
 })
